e4f96ae2e729652bef06da958a64cb90a8e5f044
GabrieleDulke <gabriele.dulke@unity3d.com>
2021-07-21 13:49:02 +0300

    weapon pickup

--- Assets/Gabriele/Scripts/PlayerMovement.cs
+++ Assets/Gabriele/Scripts/PlayerMovement.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5.0f;
+    private bool moving = false;
+    private Animator anim;
 
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     void Update()
     {
         Movement();
+        anim.SetBool("moving", moving);
     }
 
     void Movement()
@@ -18,21 +25,30 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
+            moving = true;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+            moving = true;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+            moving = true;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+            moving = true;
+        }
+
+        if(!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            moving = false;
         }
     }
 }
