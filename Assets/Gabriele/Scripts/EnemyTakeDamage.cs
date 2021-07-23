4e04b35d4bec2d0d34ded07e04de5b5ea3d14d10
GabrieleDulke <gabriele.dulke@unity3d.com>
2021-07-23 15:56:49 +0300

    added some sounds

--- Assets/Gabriele/Scripts/EnemyTakeDamage.cs
+++ Assets/Gabriele/Scripts/EnemyTakeDamage.cs
@@ -4,21 +4,51 @@ using UnityEngine;
 
 public class EnemyTakeDamage : MonoBehaviour
 {
+    private Animator anim;
+    private Collider2D coll;
+    private RandomPatrol rp;
+
+    public List<AudioSource> dieAxeSounds;
+    public List<AudioSource> dieSounds;
+
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        coll = GetComponent<Collider2D>();
+        rp = GetComponent<RandomPatrol>();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject other = collision.gameObject;
 
         if(other.CompareTag("playerBullet"))
         {
-            Destroy(gameObject);
+            Die("deadPistol", dieSounds);
+            anim.SetBool("deadPistol", true);
+            coll.enabled = false;
+            rp.enabled = false;
+            int i = Random.Range(0, dieSounds.Count);
+            dieSounds[i].Play();
+            Destroy(gameObject,3f);
         }
 
         if (other.CompareTag("Player"))
         {
             if(other.GetComponent<Animator>().GetBool("axe") && other.GetComponent<Animator>().GetBool("attack"))
             {
-                Destroy(gameObject);
+                Die("deadAxe", dieAxeSounds);
             }
         }
+    }
+
+    private void Die(string animation, List<AudioSource> audioList)
+    {
+        anim.SetBool(animation, true);
+        coll.enabled = false;
+        rp.enabled = false;
+        int i = Random.Range(0, audioList.Count);
+        audioList[i].Play();
+        Destroy(gameObject, 3f);
     }
 }
